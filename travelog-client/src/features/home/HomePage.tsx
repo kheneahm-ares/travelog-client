98fca36fabf5b3c7bf6c34a79b0c343656b1bc72
kheneahm <kheneahm9@gmail.com>
2021-03-12 12:36:48 -0600

    Add loading when logging and reg

--- travelog-client/src/features/home/HomePage.tsx
+++ travelog-client/src/features/home/HomePage.tsx
@@ -2,14 +2,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { AuthService } from "../../app/auth/AuthServices";
 import { RootState } from "../../app/store";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Button, Container, Header, Segment, Image } from "semantic-ui-react";
 import { openModal } from "../travelplans/details/activities/activitySlice";
 
 export const HomePage = () => {
   const user = useSelector((state: RootState) => state.authReducer.user);
   const doesAppHaveToken = AuthService.hasToken();
+  const [loading, setLoading] = useState({
+    signingIn: false,
+    registering: false,
+  });
 
+  function handleSignIn() {
+    setLoading({ signingIn: true, registering: false });
+    AuthService.signInRedirect();
+  }
+  function handleRegister() {
+    setLoading({ signingIn: false, registering: true });
+    AuthService.registerUser();
+  }
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -32,16 +44,20 @@ export const HomePage = () => {
           <Fragment>
             <Header as="h2" inverted content={`Welcome to Travelog`} />
             <Button
-              onClick={AuthService.signInRedirect}
+              onClick={handleSignIn}
               size="huge"
               inverted
+              loading={loading.signingIn}
+              disabled={loading.registering || loading.signingIn}
             >
               Login
             </Button>
             <Button
               size="huge"
               inverted
-              onClick={AuthService.registerUser}
+              onClick={handleRegister}
+              loading={loading.registering}
+              disabled={loading.registering || loading.signingIn}
             >
               Register
             </Button>
