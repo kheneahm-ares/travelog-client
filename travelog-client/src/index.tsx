4cf6e5379f31d18cc2fb86113d3fdb44adc5ef52
kheneahm <kheneahm9@gmail.com>
2021-05-11 19:58:07 -0500

    switched history back to no reload

--- travelog-client/src/index.tsx
+++ travelog-client/src/index.tsx
@@ -12,7 +12,7 @@ import "react-widgets/dist/css/react-widgets.css";
 import dateFnsLocalizer from "react-widgets-date-fns";
 
 dateFnsLocalizer();
-export const history = createBrowserHistory({forceRefresh: true});
+export const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={storeConfigs().store}>
